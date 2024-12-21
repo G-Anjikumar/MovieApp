0facbd90825496d24c3833a725102ddf38ae457b
anjikumar <anjikumarg@yupptv.com>
2024-12-21 12:40:05 +0530

    On Event() removed from Test case class

--- app/src/androidTest/java/com/llyod/sample/HorizontalItemUITestCase.kt
+++ app/src/androidTest/java/com/llyod/sample/HorizontalItemUITestCase.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import com.llyod.sample.ui.home.presentation.components.HorizontalCastItem
-import com.llyod.sample.utils.Util
 import org.junit.Rule
 import org.junit.Test
 
